4c81622160f80ef62e715072a032ff88fa2b3607
Larissa Nascimento <120027246+larissaNa@users.noreply.github.com>
2025-07-30 20:27:04 -0300

    vinculando a tela de nutricionista ao banco de dados

--- backend/src/controllers/clientController.ts
+++ backend/src/controllers/clientController.ts
@@ -4,26 +4,26 @@ import { Usuario } from '../models/Usuario';
 import { Op } from 'sequelize';
 
 export const buscarClientes = async (req: Request, res: Response) => {
-    const { nome } = req.query;
+  const { nome } = req.query;
 
-    try {
-        const clientes = await Cliente.findAll({
-            include: [{
-                model: Usuario,
-                as: 'usuario',
-                where: {
-                    nome: { [Op.iLike]: `%${nome}%` }
-                },
-                attributes: ['nome']
-            }],
-            attributes: ['id_cliente', 'idade', 'peso_atual', 'altura', 'restricoes', 'objetivo']
-        });
+  try {
+    const clientes = await Cliente.findAll({
+      include: [
+        {
+          model: Usuario,
+          as: 'usuario',
+          where: nome ? {
+            nome: { [Op.iLike]: `%${nome}%` }
+          } : undefined,
+          attributes: ['nome', 'email']
+        }
+      ],
+      attributes: ['id_cliente', 'idade', 'peso_atual', 'altura', 'restricoes', 'objetivo']
+    });
 
-
-
-        res.json(clientes);
-    } catch (error) {
-        console.error('Erro ao buscar clientes:', error);
-        res.status(500).json({ erro: 'Erro ao buscar clientes.' });
-    }
+    res.json(clientes);
+  } catch (error) {
+    console.error('Erro ao buscar clientes:', error);
+    res.status(500).json({ erro: 'Erro ao buscar clientes.' });
+  }
 };
